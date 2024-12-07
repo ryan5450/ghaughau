46c8116bb495cfa6a0f105e10e4a43cac135231a
Shibam Chakraborty <shibam.chakraborty@g.bracu.ac.bd>
2024-12-07 23:56:19 +0600

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@ import Navbar from "../../components/Navbar";
 import Hero from "../../components/Hero";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar2 from "../../components/Navbar2";
+import Footer from "../../components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar/>
       <Navbar2/>
       <Hero/>
+      <Footer/>
     </div>
     
   );
--- src/app/policy/page.jsx
+++ src/app/policy/page.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const Contact = () => {
+const Policy = () => {
   return (
       <div style={{ fontFamily: 'Acme, sans-serif' }} className="bg-[#F1DBE2] min-h-screen">
         <h1 className="text-center text-4xl font-bold text-[#8174A0] py-8">
-          Contact Us
+          Policy
         </h1>
       </div>
-  
   );
 };
 
-export default Contact;
+export default Policy;
