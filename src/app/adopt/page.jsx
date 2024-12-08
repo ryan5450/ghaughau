7aedda18d99bd2597d7443b38d043e4e4d55c950
Ryan Azim <ryanazimr50@gmail.com>
2024-12-08 09:41:10 +0600

    stash

--- src/app/adopt/page.jsx
+++ src/app/adopt/page.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../../../components/Navbar';
 
 const Adopt = () => {
   return (
     <div className="bg-[#F1DBE2] min-h-screen">
+      <Navbar/>
       <h1 className="text-center text-4xl font-bold text-[#8174A0] py-8">Adopt from here</h1>
     </div>
   );
