8137c7b50c832ba7e0c6fb8e273353f908c8ccd8
shibamchk10 <shibamchk@gmail.com>
2024-12-11 13:59:57 +0600

    Added pet-view

--- src/app/adopt/page.jsx
+++ src/app/adopt/page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Adopt = () => {
   return (
-    <div style={{ fontFamily: 'Acme, sans-serif' }} className="bg-[#F1DBE2] min-h-screenx">
+    <div style={{ fontFamily: 'Acme, sans-serif' }} className="bg-[#F1DBE2] min-h-screen">
       <h1 className="text-center text-4xl font-bold text-[#8174A0] py-8">
         Adopt from here
       </h1>
